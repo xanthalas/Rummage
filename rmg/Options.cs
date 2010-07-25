91480fa06bf0706aa6282cd6df04aa1658233c82
Xanthalas <xan@xanthalas.co.uk>
2010-07-25 15:55:02 +0100

    Implement a basic command-line client

---
+++ rmg/Options.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using CommandLine;
+
+namespace rmg
+{
+    class Options
+    {
+        [OptionArray("s", "search", Required = true, HelpText = "Regular Expression to search for.")]
+        public string[] SearchStrings = null;
+
+        [OptionArray("f", "folder", Required = false, HelpText = "The folder to search.")]
+        public string[] Folders;
+
+        [OptionArray("x", "excludefile", Required = false, HelpText = "Exclude files whose name matches this regex.")]
+        public string[] ExcludeFiles;
+
+        [OptionArray("i", "includefile", Required = false, HelpText = "Include only those files whose name matches this regex.")]
+        public string[] IncludeFiles;
+
+        [Option("c", "caseSensitive", Required = false, HelpText = "Make the search case-sensitive.")]
+        public bool CaseSensitive = false;
+
+        [Option("v", "verbose", Required = false, HelpText = "Show additional information during the search.")]
+        public bool Verbose = false;
+
+        /*
+        [HelpOption("Display this help screen.", "help")]
+        public string GetUsage()
+        {
+            var usage = new StringBuilder();
+            usage.AppendLine("Rummage search tool v 0.1");
+            Console.WriteLine(usage.ToString());
+            return "";
+        }
+         * */
+
+    }
+}
---
+++ rmg/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using CommandLine;
+using RummageCore;
+using RummageFilesystem;
+
+namespace rmg
+{
+    class Program
+    {
+        private static bool _verbose = false;
+
+        static void Main(string[] args)
+        {
+            ISearchRequest searchRequest = new SearchRequestFilesystem();
+
+            parseOptions(searchRequest, args);
+
+            if (_verbose) {Console.WriteLine("Starting search.");}
+
+            searchRequest.Prepare();
+            if (searchRequest.IsPrepared)
+            {
+                if (_verbose) { Console.WriteLine("{0} files will be searched", searchRequest.URL.Count); }
+                ISearch search = new SearchFilesystem();
+                search.Search(searchRequest);
+                
+                foreach (IMatch match in search.Matches)
+                {
+                    Console.WriteLine("{0}:{1} {2}", match.MatchItem, match.MatchLineNumber, match.MatchLine);
+                }
+                if (_verbose) { Console.WriteLine("Search complete. Found {0} matches.", search.Matches.Count); }
+            }
+            else
+            {
+                Console.WriteLine("Not enough information has been supplied to perform the search.");
+            }
+        }
+
+        private static void parseOptions(ISearchRequest searchRequest, string[] args)
+        {
+
+            var options = new Options();
+            ICommandLineParser parser = new CommandLineParser();
+            if (parser.ParseArguments(args, options))
+            {
+                if (options.SearchStrings != null)
+                {
+                    foreach (var searchString in options.SearchStrings)
+                    {
+                        searchRequest.SearchStrings.Add(searchString);
+                    }
+                }
+
+                if (options.Folders != null)
+                {
+                    foreach (var folder in options.Folders)
+                    {
+                        searchRequest.SearchContainers.Add(folder);
+                    }
+
+                }
+
+                if (options.IncludeFiles != null)
+                {
+                    foreach (var inc in options.IncludeFiles)
+                    {
+                        searchRequest.IncludeItemStrings.Add(inc);
+                    }
+
+                }
+
+                if (options.ExcludeFiles != null)
+                {
+                    foreach (var exc in options.ExcludeFiles)
+                    {
+                        searchRequest.IncludeItemStrings.Add(exc);
+                    }
+
+                }
+
+                searchRequest.CaseSensitive = options.CaseSensitive;
+                _verbose = options.Verbose;
+            }
+        }
+    }
+}
