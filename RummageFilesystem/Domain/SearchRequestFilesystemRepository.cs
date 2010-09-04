eaad7a665ef2316326f1275fbb369b71de293f4c
Xanthalas <xan@xanthalas.co.uk>
2010-09-04 12:08:41 +0100

    Revert "Tidy up the initial NHibernate code."
    This reverts commit 47c20163c584199a1ea42b7e8e3752efbc54348b.

---
+++ RummageFilesystem/Domain/SearchRequestFilesystemRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using RummageCore.Domain;
+
+namespace RummageFilesystem.Domain
+{
+    public interface ISearchRequestFilesystemRepository
+    {
+        void Add(ISearchRequest request);
+
+        ISearchRequest GetById(int requestId);
+    }
+}
