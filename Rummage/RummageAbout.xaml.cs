f496763b45fc442c910066dd644bfc2f62c275c5
Xanthalas <xanthalas@live.co.uk>
2014-10-02 16:10:13 +0100

    Add hyperlink to repository on Github to About box

--- Rummage/RummageAbout.xaml.cs
+++ Rummage/RummageAbout.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Windows;
@@ -36,6 +37,14 @@ namespace Rummage
             {
                 Close();
             }
+        }
+
+        private void Hyperlink_RequestNavigate(object sender, System.Windows.Navigation.RequestNavigateEventArgs e)
+        {
+            if (sender.GetType() != typeof(Hyperlink))
+                return;
+            string link = ((Hyperlink)sender).NavigateUri.ToString();
+            Process.Start(link);
         }
     }
 }
